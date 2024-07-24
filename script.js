71e99ecd448669b54009d87bbc479f1fe2d2d52b
eha Sebastian <nehasebastian@gmail.com>
2024-07-24 15:50:07 +0530

    First Day

---
+++ script.js
@@ -0,0 +1,36 @@
+//const button = document.getElementById('eventbtn');
+//const text = document.getElementById('text');
+
+//button.addEventListener('click',function(){
+   // alert('button is clicked')
+   // text.innerHTML='click to Alert'
+//});
+
+// const persons={
+//     firstname:"Neha",
+//     lastname:"Sebastian",
+//     age:22,
+//     eyecolr:'blue'
+
+// }
+
+// console.log(persons.firstname+" "+"is"+" "+persons.age+"  years old")
+
+// for(let i=0; i<=10; i++){
+//     console.log(i)
+// }
+
+// function square(number)
+// {
+//     return number*number;
+// }
+
+// console.log(square(6));
+
+// let sum=(a,b)=>
+// {
+//      c=a+b;
+//      return c;
+// }
+// console.log(sum(2,5));
+
